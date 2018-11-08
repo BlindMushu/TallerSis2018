bc431293b828abf5bce66fa5926916077eff0f99
Kevin <kaeskevin@gmail.com>
2018-11-08 03:37:17 -0400

    Cuidador

--- taller2018/resources/views/auth/register.blade.php
+++ taller2018/resources/views/auth/register.blade.php
@@ -199,77 +199,109 @@
                                 <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
                                     <div class="card-body">
                                         <form>
-                                            <h6 class="heading-small text-muted mb-4">User information</h6>
+                                            <h6 class="heading-small text-muted mb-4">Informacion de usuario</h6>
                                             <div class="pl-lg-4">
                                                 <div class="row">
                                                     <div class="col-lg-6">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-username">Username</label>
-                                                            <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse">
+                                                            <label class="form-control-label" for="nombre">Nombre</label>
+                                                            <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Nombre">
                                                         </div>
                                                     </div>
                                                     <div class="col-lg-6">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-email">Email address</label>
-                                                            <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com">
+                                                            <label class="form-control-label" for="apellido">Apellidos</label>
+                                                            <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Apellido">
                                                         </div>
-                                                    </div>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col-lg-6">
+                                                    <div class="col-lg-4">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-first-name">First name</label>
-                                                            <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky">
+                                                            <label class="form-control-label" for="correo">Correo electronico</label>
+                                                            <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="correo@ejemplo.com">
                                                         </div>
                                                     </div>
-                                                    <div class="col-lg-6">
+                                                    <div class="col-lg-4">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-last-name">Last name</label>
-                                                            <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse">
+                                                            <label class="form-control-label" for="password">Contraseña</label>
+                                                            <input type="password" id="password" class="form-control form-control-alternative" placeholder="Contraseña">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-4">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="tipo_usuario">Tipo de usuario</label>
+                                                            <input type="text" id="tipo_usuario" class="form-control form-control-alternative" value="Cuidador" disabled>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                             <hr class="my-4" />
                                             <!-- Address -->
-                                            <h6 class="heading-small text-muted mb-4">Contact information</h6>
+                                            <h6 class="heading-small text-muted mb-4">Informacion de contacto</h6>
                                             <div class="pl-lg-4">
                                                 <div class="row">
-                                                    <div class="col-md-12">
+                                                    <div class="col-lg-3">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-address">Address</label>
-                                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                                                            <label class="form-control-label" for="departamento">Departamento</label>
+                                                            <input type="text" id="departamento" class="form-control form-control-alternative" placeholder="Departamento">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="zona">Zona</label>
+                                                            <input type="text" id="zona" class="form-control form-control-alternative" placeholder="Zona">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="calle">Calle</label>
+                                                            <input type="text" id="calle" class="form-control form-control-alternative" placeholder="Calle">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="numero_puerta">Numero de puerta</label>
+                                                            <input type="text" id="numero_puerta" class="form-control form-control-alternative" placeholder="#">
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
-                                                    <div class="col-lg-4">
+                                                    <div class="col-md-4">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-city">City</label>
-                                                            <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
+                                                            <label class="form-control-label" for="direccion">Direccion</label>
+                                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Direccion" >
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="telefono">Telefono</label>
+                                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Telefono" >
                                                         </div>
                                                     </div>
                                                     <div class="col-lg-4">
                                                         <div class="form-group">
-                                                            <label class="form-control-label" for="input-country">Country</label>
-                                                            <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-lg-4">
-                                                        <div class="form-group">
-                                                            <label class="form-control-label" for="input-country">Postal code</label>
-                                                            <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
+                                                            <label class="form-control-label" for="estadodecuenta">Estado de Cuenta</label>
+                                                            <input type="text" id="tipo_usuario" class="form-control form-control-alternative" value="Solicitando" disabled>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                             <hr class="my-4" />
-                                            <!-- Description -->
-                                            <h6 class="heading-small text-muted mb-4">About me</h6>
+                                            <!-- Address -->
+                                            <h6 class="heading-small text-muted mb-4">Experiencias</h6>
                                             <div class="pl-lg-4">
-                                                <div class="form-group">
-                                                    <label>About Me</label>
-                                                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="aniosdetrabajo">Años de Trabajo</label>
+                                                            <input type="text" id="departamento" class="form-control form-control-alternative" placeholder="#">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="referencias">Referencias Laborales</label>
+                                                            <input type="text" id="zona" class="form-control form-control-alternative" placeholder="Referencias Laborales">
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </form>
